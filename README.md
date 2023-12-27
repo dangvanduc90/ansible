3f2f715ef33b827b4230dd469f61e63363c6830c
ducdvdls <ducdv@dlsinc.com>
2023-12-27 10:34:04 +0700

    Revert "Initial"
    This reverts commit 4b5723162fc124a053f09a177d433eec287cf8d4.

--- README.md
+++
@@ -1,6 +0,0 @@
-copy noi dung file hosts vao day
-vi /etc/ansible/hosts
-
-
-run command duoi de xuat file ssh config
-`vagrant ssh-config > vagrant-ssh`
--- basic.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-sudo yum install tar wget unzip vim epel-release -y 
-sudo yum install screen -y
-sudo yum update -y 
-sudo setenforce 0
-sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
-sudo reboot
